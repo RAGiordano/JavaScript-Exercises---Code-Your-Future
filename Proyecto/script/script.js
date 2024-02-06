d4385caefcfb4bc785aa0c82f3c87c4b614d9cc6
RAGiordano <profesorgiordano@gmail.com>
2024-02-06 10:10:16 -0300

    Arrays - Task1

--- Proyecto/script/script.js
+++ Proyecto/script/script.js
@@ -63,3 +63,87 @@ function buy5(event) {
   stock5.textContent = "Stock: " + productStock;
   quantity.focus();
 }
+
+// TASK 6
+function secretNumber6(){
+  let secretDiscountCode = "TOPSECRET";
+  let codeAttempts = 0;
+  let codeInput = "";
+  let maxAttempts = 5;
+
+  do {
+    codeAttempts ++;
+    codeInput = prompt("Attempt " + codeAttempts + " - Enter code:");
+    
+  } while (codeInput != secretDiscountCode && codeAttempts < maxAttempts)
+
+  if (codeInput == secretDiscountCode) {
+    alert("Code entered successfully")
+  } else {
+    alert("Maximum attempts reached");
+  }
+}
+
+
+// TASK 7
+function convert7(event){
+  event.preventDefault();
+  let rate = getElementById("rate7");
+  let amount = getElementById("amount7");
+  let convertion = getElementById("convertion7");
+  convertion.textContent = parseFloat(amount.value) / rate;
+}
+
+// TASK LIST
+document.addEventListener('DOMContentLoaded', function () {
+  let taskInput = document.getElementById('taskInput');
+  let taskList = document.getElementById('taskList');
+
+  function addTask() {
+      let taskText = taskInput.value.trim();
+      
+      if (taskText !== '') {
+          let newTask = document.createElement('li');
+          newTask.innerHTML = `
+              <span>${taskText}</span>
+              <button onclick="toggleTask(this)">Completed</button>
+              <button onclick="removeTask(this)">Remove</button>
+          `;
+          taskList.appendChild(newTask);
+          taskInput.value = '';
+      }
+  }
+
+  window.addTask = addTask;
+
+  window.toggleTask = function(button) {
+      let task = button.parentNode.querySelector("span");
+      task.classList.toggle('completed');
+  };
+
+  window.removeTask = function(button) {
+      let task = button.parentNode;
+      task.parentNode.removeChild(task);
+  };
+});
+
+// MODIFYING ARRAYS
+function modifyingArrays1(){
+  let originalArray = [1, 2, 3, 4];
+  // let originalArrayCopy = originalArray;
+  let originalArrayCopy = [] + originalArray;
+  let poppedElement = originalArray.pop();
+  let shiftedElement = originalArray.shift();
+
+
+  let resultContainer = document.getElementById('array1result');
+  let array1result = document.createElement('div');
+          array1result.innerHTML = `
+              <h3 style="margin: 30px 0">Result</h3>
+              <p>Original array: ${originalArrayCopy}</p>
+              <p>Popped element: ${poppedElement}</p>
+              <p>Shifted element: ${shiftedElement}</p>
+              <p>My final array: ${originalArray}</p>
+          `;
+  resultContainer.appendChild(array1result);
+}
